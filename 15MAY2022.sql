a25faef1eb6c61f2641f2fb18b8be14fc2661aaf
Madhavan1512 <45940540+Madhavan1512@users.noreply.github.com>
2022-05-19 10:37:39 +0530

    Update 15MAY2022.sql

--- 15MAY2022.sql
+++ 15MAY2022.sql
@@ -1,1 +1,3 @@
-
+Select sell_date, Count(Distinct product) As num_sold, Group_Concat(Distinct product Order By product Separator ',') As products From Activities
+Group By sell_date
+Order By sell_date;
