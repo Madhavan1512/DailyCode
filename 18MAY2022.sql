164b0dd2fbf56dfbc11a6a01af3be7786d6b95c4
Madhavan1512 <45940540+Madhavan1512@users.noreply.github.com>
2022-05-19 10:42:57 +0530

    Update 18MAY2022.sql

--- 18MAY2022.sql
+++ 18MAY2022.sql
@@ -1,1 +1,5 @@
-
+Select product_id, 'store1' As store, store1 As price From Products Where store1 Is Not null
+UNION
+Select product_id, 'store2' As store, store2 As price From Products Where store2 Is Not null
+UNION
+Select product_id, 'store3' As store, store3 As price From Products Where store3 Is Not null;
